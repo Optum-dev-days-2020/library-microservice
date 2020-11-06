efe5fa7901c736ba4d2f68cf990680aaf6acdfd4
Siavash <siavash.sedghi@optum.com>
2020-11-05 19:43:28 -0600

    init commit

---
+++ src/main/java/com/optum/pdp/libraryService/LibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optum.pdp.libraryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServiceApplication.class, args);
+	}
+
+}
