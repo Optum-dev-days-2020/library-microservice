fd737db903a4127e282449f14f81d3bfb1a9492e
Siavash <siavash.sedghi@optum.com>
2020-11-06 01:12:27 -0600

    Add Post endpoint to add new books

--- src/main/java/com/optum/pdp/libraryService/controller/BookController.java
+++ src/main/java/com/optum/pdp/libraryService/controller/BookController.java
@@ -1,25 +1,33 @@
 package com.optum.pdp.libraryService.controller;
 
 import com.optum.pdp.libraryService.model.DTO.BookDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/book")
 public class BookController {
 
-    private List<BookDTO> bookEntities = List.of(
+    private List<BookDTO> bookEntities = new ArrayList<>(Arrays.asList(
             new BookDTO("1", "Brave New World", "Aldous Huxley", "sci-fi"),
             new BookDTO("2", "Modern Java in Action", "Alan Mycroft", "tech"),
             new BookDTO("3", "Design Pattern", "Eric Gamma", "tech"),
             new BookDTO("4", "Nineteen Eighty-Four", "George Orwell", "fictional"),
-            new BookDTO("5", "A Brief History of Time", "Stephen Hawking", "non-fictional")
+            new BookDTO("5", "A Brief History of Time", "Stephen Hawking", "non-fictional"))
     );
 
 
-    @GetMapping("/books")
+    @GetMapping
     public List<BookDTO> getBookList() {
         return bookEntities;
+    }
+
+    @PostMapping
+    public BookDTO addBook(@RequestBody BookDTO newBook) {
+        bookEntities.add(newBook);
+        return newBook;
     }
 }
